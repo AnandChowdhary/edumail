3e5c61af71df7c69fc41f672442398a2acf7d424
Anand Chowdhary <mail@anandchowdhary.com>
2019-05-26 22:23:26 +0200

    :sparkles: Add cached read file

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import { join } from "path";
 import { readFile } from "fs-extra";
+import NodeCache from "node-cache";
 
 const domainsDirectory = join(__dirname, "..", "swot", "lib", "domains");
 
+const cache = new NodeCache();
+const cachedReadFile = async (file: string) => {
+  const key = `file_${file}`;
+  const result: string | undefined = cache.get(key);
+  if (result) return result;
+  return (await readFile(file)).toString();
+}
+
 export const isBlackListed = async (email: string) => {
-  const blacklist = (await readFile(join(domainsDirectory, "blacklist.txt")))
-    .toString()
-    .split("\n");
+  const blacklist = (await cachedReadFile(join(domainsDirectory, "blacklist.txt"))).split("\n");
   let isBlackListed = false;
   blacklist.forEach(
     domain => (isBlackListed = isBlackListed || email.endsWith(domain))
