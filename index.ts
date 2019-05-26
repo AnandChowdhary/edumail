b3c170058f58ae6a14d90c9d942a5432583004bb
Anand Chowdhary <mail@anandchowdhary.com>
2019-05-26 22:16:56 +0200

    :construction: Add blacklist check

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello!");
+import { join } from "path";
+import { readFile } from "fs-extra";
+
+const domainsDirectory = join(__dirname, "..", "swot", "lib", "domains");
+
+export const isBlackListed = async (email: string) => {
+  const blacklist = (await readFile(join(domainsDirectory, "blacklist.txt")))
+    .toString()
+    .split("\n");
+  let isBlackListed = false;
+  blacklist.forEach(
+    domain => (isBlackListed = isBlackListed || email.endsWith(domain))
+  );
+  return isBlackListed;
+};
+
+isBlackListed("anandchowdhary@gmail.com").then(blacklisted => {
+  console.log("Is blacklisted?", blacklisted);
+});
