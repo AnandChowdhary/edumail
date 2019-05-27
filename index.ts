50550b994fe47f2bfd6cc24bf15c73e9d270c553
Anand Chowdhary <mail@anandchowdhary.com>
2019-05-27 08:32:09 +0200

    :sparkles: Add support for checking email

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { join } from "path";
-import { readFile } from "fs-extra";
+import { readFile, remove } from "fs-extra";
 import NodeCache from "node-cache";
 
-const domainsDirectory = join(__filename, "..", "swot", "lib", "domains");
+const domainsDirectory = join(__dirname, "swot", "lib", "domains");
 
 const cache = new NodeCache();
 const cachedReadFile = async (file: string) => {
@@ -35,4 +35,34 @@ export const isEducationalDomain = async (email: string) => {
         isEducationalDomain || !!(domain && email.endsWith(domain)))
   );
   return isEducationalDomain;
+};
+
+const hasFile = async (fileName: string) => {
+  try {
+    await readFile(fileName);
+    return true;
+  } catch (error) {}
+  return false;
+};
+
+const removeAfter = (array: string[], val: string) => {
+  const index = array.indexOf(val);
+  array.splice(index + 1, array.length - (index + 1));
+  return array;
+};
+
+export const isEducational = async (email: string) => {
+  if ((await isEducationalDomain(email)) && !(await isBlackListed(email)))
+    return true;
+  const domain = email
+    .split("@")[1]
+    .split(".")
+    .reverse();
+  for await (const domainPart of domain) {
+    const newArray = removeAfter([...domain], domainPart);
+    newArray[newArray.length - 1] = `${newArray[newArray.length - 1]}.txt`;
+    const has = await hasFile(join(domainsDirectory, ...newArray));
+    if (has) return true;
+  }
+  return false;
 };
